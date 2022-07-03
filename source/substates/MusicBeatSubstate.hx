b60ba252af91c1cec03a294d154db31da6cb1678
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-07-03 18:39:10 +0800

    bruh

--- source/substates/MusicBeatSubstate.hx
+++ source/substates/MusicBeatSubstate.hx
@@ -35,7 +35,7 @@ class MusicBeatSubstate extends FlxSubState
 		return PlayerSettings.player1.controls;
 
 	#if android
-        _virtualpad:FlxVirtualPad;
+        var _virtualpad:FlxVirtualPad;
 	var trackedinputsUI:Array<FlxActionInput> = [];
 	#end
 	
