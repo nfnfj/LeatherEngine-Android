ebfe0424322b0e6d5923a9c6514f740cbd72c99c
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-07-03 14:24:35 +0800

    Update MusicBeatState.hx

--- source/states/MusicBeatState.hx
+++ source/states/MusicBeatState.hx
@@ -38,6 +38,45 @@ class MusicBeatState extends FlxUIState
 		super();
 	}
 
+        #if android
+	var _virtualpad:FlxVirtualPad;
+	var _hitbox:FlxHitbox;
+	var trackedinputsUI:Array<FlxActionInput> = [];
+	var trackedinputsNOTES:Array<FlxActionInput> = [];
+	#end
+	#if android
+	public function addVirtualPad(DPad:FlxDPadMode, Action:FlxActionMode) {
+		_virtualpad = new FlxVirtualPad(DPad, Action);
+		add(_virtualpad);
+		controls.setVirtualPadUI(_virtualpad, DPad, Action);
+		trackedinputsUI = controls.trackedinputsUI;
+		controls.trackedinputsUI = [];
+	}
+	#end
+		
+	#if android
+	public function removeVirtualPad() {
+		controls.removeFlxInput(trackedinputsUI);
+		remove(_virtualpad);
+	}
+	#end
+
+	#if android
+        public function addPadCamera() {
+		var camcontrol = new flixel.FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		_virtualpad.cameras = [camcontrol];
+	}
+	#end
+	
+	override function destroy() {
+		#if android
+		controls.removeFlxInput(trackedinputsUI);
+		controls.removeFlxInput(trackedinputsNOTES);	
+		#end
+	}
+
 	override function update(elapsed:Float)
 	{
 		//everyStep();
