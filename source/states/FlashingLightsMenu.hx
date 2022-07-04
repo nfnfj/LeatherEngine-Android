2f6ec19cf17c612a1747f92f7affb20931993636
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-07-04 10:37:34 +0800

    Update FlashingLightsMenu.hx

--- source/states/FlashingLightsMenu.hx
+++ source/states/FlashingLightsMenu.hx
@@ -32,5 +32,7 @@ class FlashingLightsMenu extends MusicBeatState
 
         if(controls.BACK)
             FlxG.switchState(new TitleState());
+	else if(controls.ACCEPT)
+	    FlxG.switchState(new TitleState());
     }
 }
