aea2e6bca5caf6d983e970a60d4d53999bffb395
Raiden Alfares 100% REAL <97826418+RaidenAlfares12@users.noreply.github.com>
2022-07-04 10:17:31 +0700

    I forgot

--- source/ui/SimpleInfoDisplay.hx
+++ source/ui/SimpleInfoDisplay.hx
@@ -72,6 +72,9 @@ class SimpleInfoDisplay extends TextField
                         case 2:
                             // Version
                             version_Function();
+                        case 3:
+                           // watermark
+                            watermark_Function();
                     }
 
                     text += "\n";
