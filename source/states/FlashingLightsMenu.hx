e6b0cf075398193100d6976997d431b42ecd3f54
kamodrip <100513117+Akhia11@users.noreply.github.com>
2022-07-04 18:51:08 +0300

    Update FlashingLightsMenu.hx

--- source/states/FlashingLightsMenu.hx
+++ source/states/FlashingLightsMenu.hx
@@ -1,16 +1,8 @@
 package states;
-
-#if android
-import android.Tools;
-#end
 import utilities.Options;
 import flixel.FlxG;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
-import sys.FileSystem;
-import sys.io.File;
-import flash.system.System;
-
 class FlashingLightsMenu extends MusicBeatState
 {
     override public function create()
@@ -19,24 +11,19 @@ class FlashingLightsMenu extends MusicBeatState
 	addVirtualPad(NONE, A_B);
         #end
         super.create();
-
         var text = new FlxText(0,0,0,"Hey! Leather Engine has flashing lights\nPress A to enable them, or B to not.\n(Any key closes this menu)", 32);
         text.font = Paths.font("vcr.ttf");
         text.screenCenter();
         text.setBorderStyle(OUTLINE, FlxColor.BLACK, 1.5, 1);
         add(text);
     }
-
     override function update(elapsed:Float)
     {
         super.update(elapsed);
-
         if(controls.ACCEPT)
             Options.setData(true, "flashingLights");
-         SUtil.applicationAlert('Flashling Enabled!');
         else if(!controls.ACCEPT && controls.BACK)
             Options.setData(false, "flashingLights");
-           SUtil.applicationAlert('Flashling Disabled! !');
 
         if(controls.BACK)
             FlxG.switchState(new TitleState());
