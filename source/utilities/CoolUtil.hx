ce8d32fbd131a9c567c7cb550c3e2840caf1cfb4
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-07-08 18:39:06 +0800

    Hmm

--- source/utilities/CoolUtil.hx
+++ source/utilities/CoolUtil.hx
@@ -34,7 +34,7 @@ class CoolUtil
 
 	public static function coolTextFile(path:String):Array<String>
 	{
-		var daList:Array<String> = File.getContent(path).trim().split('\n');
+		var daList:Array<String> = File.getContent(SUtil.getPath() + (path).trim().split('\n'));
 
 		for (i in 0...daList.length)
 		{
