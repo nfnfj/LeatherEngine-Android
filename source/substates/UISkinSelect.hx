1031b88446c85d6169648793262a4bd829d79364
Raiden Alfares 100% REAL <97826418+RaidenAlfares12@users.noreply.github.com>
2022-07-20 12:49:43 +0700

    Update UISkinSelect.hx

--- source/substates/UISkinSelect.hx
+++ source/substates/UISkinSelect.hx
@@ -12,6 +12,8 @@ import flixel.group.FlxGroup;
 import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.FlxSprite;
+import.android.flixel.FlxVirtualPad
+// import.fuckin.i.hate.this.menu
 
 class UISkinSelect extends MusicBeatSubstate
 {
@@ -105,6 +107,7 @@ class UISkinSelect extends MusicBeatSubstate
             });
         }
 
+
         if(left || right && !leaving)
         {
             if(left)
@@ -124,6 +127,10 @@ class UISkinSelect extends MusicBeatSubstate
 
             current_UI_Skin.text = "Selected Skin: > " + ui_Skin + " <";
         }
+
+                #if android
+	        addVirtualPad(UP_DOWN, A_B);
+                #end
 
         if(accepted && !leaving)
             utilities.Options.setData(ui_Skin, "uiSkin");
