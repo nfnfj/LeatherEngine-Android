6865d89ee969b3a2c1993a94af0c5d5ae96db12e
Raiden Alfares 100% REAL <97826418+RaidenAlfares12@users.noreply.github.com>
2022-08-13 08:41:57 +0700

    Ah

--- source/substates/ResultsScreenSubstate.hx
+++ source/substates/ResultsScreenSubstate.hx
@@ -85,7 +85,7 @@ class ResultsScreenSubstate extends MusicBeatSubstate
         @:privateAccess
         if(FlxG.keys.justPressed.SHIFT && !PlayState.playingReplay && !PlayState.instance.hasUsedBot #if android || _virtualpad.buttonB.justPressed #end) 
             PlayState.instance.saveReplay();
-            if(!FileSystem.exists(saveReplay))
+            if(!FileSystem.exists(savePath))
 
         @:privateAccess
         if(FlxG.keys.justPressed.ESCAPE && !PlayState.playingReplay && !PlayState.instance.hasUsedBot #if android || _virtualpad.buttonC.justPressed #end)
