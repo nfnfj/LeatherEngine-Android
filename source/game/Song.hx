5648d3c8b77e1153272febf60b4a589786f92c7b
Raiden Alfares 100% REAL <97826418+RaidenAlfares12@users.noreply.github.com>
2022-08-17 10:43:10 +0700

    Update Song.hx
    Uhmm

--- source/game/Song.hx
+++ source/game/Song.hx
@@ -96,7 +96,7 @@ class Song
 
 		var rawJson:String = "";
 
-		rawJson = SUtil.getPath() + Paths.json(folder.toLowerCase() + jsonInput.toLowerCase()).trim();
+		rawJson = Assets.getText(Paths.json(folder.toLowerCase() + jsonInput.toLowerCase())).trim();
 
 		if(rawJson != "")
 		{
@@ -110,7 +110,7 @@ class Song
 		}
 		else
 		{
-			rawJson = SUtil.getPath() + Paths.json("song data/tutorial/tutorial")).trim();
+			rawJson = Assets.getText(Paths.json("song data/tutorial/tutorial")).trim();
 
 			while (!rawJson.endsWith("}"))
 			{
